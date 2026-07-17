dbd0fc2841a81f308b58ab9b6838b37ff9a4469b
Dmitrii Gribovskii <dmitrii.gribovskii@crezu.com>
2026-07-18 04:10:08 +0500

    Update

--- content.js
+++ content.js
@@ -1,29 +1,84 @@
-const scaleAndCopyImage = (imageUrl) => {
-  if (!imageUrl) return
+const getImageBlobByImageObject = (image, size) => {
+  return new Promise(async (resolve) => {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    canvas.width = size.width
+    canvas.height = size.height
+    ctx.drawImage(image, 0, 0, size.width, size.height)
+    try {
+      canvas.toBlob(async (blob) => resolve(blob))
+    } catch (e) {
+      console.error('Не удалось получить Blob через Image Object', e)
+      resolve(null)
+    }
+  })
+}
+
+const getImageBlobBySrcUrl = (imageSrc, size) => {
+  return new Promise(async (resolve) => {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const image = new Image()
+    image.crossOrigin = 'anonymous'
+    image.src = imageSrc
+
+    image.onload = async () => {
+      canvas.width = size.width
+      canvas.height = size.height
+      ctx.drawImage(image, 0, 0, size.width, size.height)
+      try {
+        canvas.toBlob(async (blob) => resolve(blob))
+      } catch (e) {
+        console.error('Не удалось получить Blob через Image Url', e)
+        resolve(null)
+      }
+    }
+  })
+}
+
+
+const scaleAndCopyImage = async (image) => {
+  if (!image) return
+  const size = await getScaledSizeOfImage(image)
+  let imageBlob = await getImageBlobByImageObject(image, size)
+  if (!imageBlob) imageBlob = await getImageBlobBySrcUrl(image.src, size)
+  if (!imageBlob) {
+    const error = 'Не удалось получить Blob изображения'
+    alertify.error(error, 6)
+    console.error(error)
+    return
+  }
+
+  try {
+    await navigator.clipboard.write([
+      new ClipboardItem({
+        [imageBlob.type]: imageBlob
+      })
+    ])
+    alertify.success(`Скопировано! ${size.width}x${size.height}`, 3)
+  } catch (e) {
+    alertify.error('Не удалось записать изображение в буфер обмена', 6)
+    console.error(e)
+  }
+}
+
+const downloadImage = async (image) => {
+  if (!image) return
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
-  const image = new Image()
-  image.crossOrigin = 'anonymous'
-  image.src = imageUrl
-
-  image.onload = async () => {
-    const { width, height } = await getScaledSizeOfImage(image)
-    canvas.width = width
-    canvas.height = height
-    ctx.drawImage(image, 0, 0, width, height)
-    canvas.toBlob(async (blob) => {
-      try {
-        await navigator.clipboard.write([
-          new ClipboardItem({
-            [blob.type]: blob
-          })
-        ])
-        alertify.success(`Скопировано! ${width}x${height}`, 3)
-      } catch (e) {
-        alertify.error('Не удалось записать изображение в буфер обмена', 6)
-      }
-    })
-  }
+  canvas.width = image.naturalWidth
+  canvas.height = image.naturalHeight
+  ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
+  canvas.toBlob(async (blob) => {
+    try {
+      const link = document.createElement('a')
+      link.download = 'filename.png'
+      link.href = canvas.toDataURL()
+      link.click()
+    } catch (e) {
+      alertify.error('Не удалось загрузить изображение', 3)
+    }
+  })
 }
 
 const getScaledSizeOfImage = async (image) => {
@@ -61,9 +116,14 @@ const getSizeSetting = () => {
   })
 }
 
+let lastClickedImageElement = null
+document.addEventListener('contextmenu', (event) => {
+  lastClickedImageElement = event.target
+}, true)
+
 chrome.runtime.onMessage.addListener((message) => {
   if (message?.name === 'start') {
-    const imageUrl = message?.data?.srcUrl
-    scaleAndCopyImage(imageUrl)
+    scaleAndCopyImage(lastClickedImageElement)
+    // downloadImage(lastClickedImageElement)
   }
 })
